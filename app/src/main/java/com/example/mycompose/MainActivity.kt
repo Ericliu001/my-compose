1145ef5ba2a235a6b3670895572b377af5d08025
Eric Liu <eric.liu@uber.com>
2021-10-24 10:10:27 -0700

    adds material design

--- app/src/main/java/com/example/mycompose/MainActivity.kt
+++ app/src/main/java/com/example/mycompose/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -38,11 +40,23 @@ fun Greeting(name: String) {
     Row {
         Image(painter = painterResource(id = R.drawable.profile_picture),
             contentDescription = "Profile Picture",
-            modifier = Modifier.size(50.dp).clip(CircleShape)
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, color = colors.secondary, CircleShape)
         )
         Column {
-            Text(text = "Hello $name!")
-            Text(text = "Welcome to App Development!")
+            Text(text = "Hello $name!", color = colors.secondaryVariant, style = typography.body1)
+
+            Spacer(modifier = Modifier.size(5.dp))
+
+            Surface(shape = shapes.medium, elevation = 1.dp) {
+
+                Text(text = "Welcome to App Development!",
+                    style = typography.body2,
+                    modifier = Modifier.padding(4.dp)
+                )
+            }
         }
     }
 }
